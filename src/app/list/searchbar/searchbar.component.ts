8919516d9c978f4bf384b8a748d0f5cc064cff90
Dzmitry Samuilik <Sambl4@tut.by>
2018-08-31 01:25:32 +0300

    refactor(searchbar component): change search behavior

--- src/app/list/searchbar/searchbar.component.ts
+++ src/app/list/searchbar/searchbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Output, NgModule, EventEmitter } from '@angular/core';
-import { Observable, interval } from 'rxjs';
+import { Observable, interval, of, Subscription, BehaviorSubject } from 'rxjs';
 
 import { ListItem } from '../../model/list-item.model';
 import { ListService } from '../list.service';
@@ -17,15 +17,30 @@ import { SearchByTitlePipe } from '../../share/pipe/my-search.pipe';
 export class SearchbarComponent implements OnInit {
   @Output() filteredArray: EventEmitter<ListItem[]> = new EventEmitter<ListItem[]>();
   @Output() searchedValue: EventEmitter<string> = new EventEmitter<string>();
-  searchValue: string;
-  private searchedArray: ListItem[] = [];
-  private source = Observable.create();
-  // private source = Observable.interval(200).take(3);
+  public searchValue: string;
+  public isSearched: boolean;
+  // private searchedArray: ListItem[] = [];
 
   constructor(private searchByTitlePipe: SearchByTitlePipe, private listService: ListService) { }
 
   ngOnInit() {
+    this.isSearched = false;
   }
+
+  changeSearchValue(value) {
+    this.searchValue = value;
+    this.isSearched = true;
+    if (this.searchValue && this.searchValue.length > 3) {
+      this.searchedValue.emit(this.searchValue);
+    }
+  }
+
+  searchingReset() {
+    this.isSearched = false;
+    this.searchValue = '';
+    this.searchedValue.emit(this.searchValue);
+  }
+
 
   search() {
     // let filteredByTitle: ListItem[];
@@ -38,7 +53,7 @@ export class SearchbarComponent implements OnInit {
     // this.filteredArray.emit(filteredByTitle);
 
     // if (this.searchValue && this.searchValue.length) {
-      this.searchedValue.emit(this.searchValue);
+      // this.searchedValue.emit(this.searchValue);
     // }
   }
 }
